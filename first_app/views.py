7f5e33080cd6455fad7d2e641884f995f8be89af
mdarafatyeasin <yeasinarafat54239@gmail.com>
2023-12-17 10:15:17 +0600

    first project

---
+++ first_app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def appHome (request) :
+    return render (request, 'appHome/appHome.html')
+
+def home (request) :
+    return render (request, 'home/home.html')
+
+def about (request) :
+    return render (request, 'about/about.html')
+
+def contact (request) :
+    return render (request, 'contact/contact.html')
